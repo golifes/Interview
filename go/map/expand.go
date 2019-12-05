fa5295d313ac603f9e37551fc193b0dafc94bd5f
fengpf <18221167541@163.com>
2019-12-05 17:52:50 +0800

    ad make map analysis

---
+++ go/map/expand.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var  (
+	// makehmap_small implements Go map creation for make(map[k]v) and
+	// make(map[k]v, hint) when hint is known to be at most bucketCnt
+	// at compile time and the map needs to be allocated on the heap.
+	b = make(map[int]int)//在堆上分配内存，调用 makemap_small
+)
+
+func main() {
+	var a map[int]int
+
+	// Only need to initialize h.hash0 since
+	// hmap h has been allocated on the stack already.
+	// h.hash0 = fastrand()
+	a = make(map[int]int) //创建map，在栈上分配直接调用 fastrand
+
+	a[1] = 1      //map 存值
+	v, ok := a[1] //map取值
+
+	fmt.Println(v, ok)
+}
+
